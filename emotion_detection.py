02653c6a76348c239ef5ef8da3d18aeb952b117d
rakati <ouhaddou.nouredine@gmail.com>
2023-11-10 14:05:42 +0100

    convert response text and return only useful info

---
+++ emotion_detection.py
@@ -0,0 +1,18 @@
+import requests
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(url, json = myobj, headers=header)
+    text = response.text
+    info_dic = json.loads(text)["emotionPredictions"][0]
+    res_dic = {
+        'anger': info_dic["emotion"]["anger"],
+        'disgust': info_dic["emotion"]["disgust"],
+        'fear': info_dic["emotion"]["fear"],
+        'joy': info_dic["emotion"]["joy"],
+        'sadness': info_dic["emotion"]["sadness"],
+    }
+    res_dic["dominant_emotion"] = max(res_dic, key=res_dic.get)
+    return res_dic
