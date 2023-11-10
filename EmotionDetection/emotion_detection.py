6bcec7455f6aa49ceb62cc8246aefa98cc58f40d
rakati <ouhaddou.nouredine@gmail.com>
2023-11-10 19:21:08 +0100

    Handle error bad request 400 to Watson NLP lib

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,6 +6,15 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=header)
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None,
+        }
     text = response.text
     info_dic = json.loads(text)["emotionPredictions"][0]
     res_dic = {
